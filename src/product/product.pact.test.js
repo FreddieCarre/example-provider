4857dc4d8cd1e8db0f536c457e09837ba1a9eb38
Freddie Carre <freddie.carre@infinityworks.com>
2020-04-22 18:22:20 +0100

    added colour field

--- src/product/product.pact.test.js
+++ src/product/product.pact.test.js
@@ -42,13 +42,13 @@ describe("Pact Verification", () => {
         stateHandlers: {
           "product with ID 10 exists": () => {
             controller.repository.products = new Map([
-                ["10", new Product("10", "CREDIT_CARD", "28 Degrees", "v1")]
+                ["10", new Product("10", "CREDIT_CARD", "28 Degrees", "v1", "red")]
             ]);
           },
           "products exist": () => {
             controller.repository.products = new Map([
-              ["09", new Product("09", "CREDIT_CARD", "Gem Visa", "v1")],
-              ["10", new Product("10", "CREDIT_CARD", "28 Degrees", "v1")]
+              ["09", new Product("09", "CREDIT_CARD", "Gem Visa", "v1", "green")],
+              ["10", new Product("10", "CREDIT_CARD", "28 Degrees", "v1", "red")]
             ]);
           },
           "no products exist": () => {
